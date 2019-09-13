a666c8ebf1b4b80ef492752a8ecfb2989c67db7e
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-09-12 22:38:23 -0600

    Update test data to match the data fetching and not just a subset of the datsets.

--- test/data.js
+++ test/data.js
@@ -79,6 +79,326 @@ let data = {
     {
       id: 20,
       name: 'Ila Wiza'
+    },
+    {
+      id: 21,
+      name: 'Francesca Daugherty'
+    },
+    {
+      id: 22,
+      name: 'Abdul Jerde'
+    },
+    {
+      id: 23,
+      name: 'Claudie Muller'
+    },
+    {
+      id: 24,
+      name: 'Johnathan Emard'
+    },
+    {
+      id: 25,
+      name: 'Bianka Russel'
+    },
+    {
+      id: 26,
+      name: 'Everett Stracke'
+    },
+    {
+      id: 27,
+      name: 'Magdalena Marks'
+    },
+    {
+      id: 28,
+      name: 'Corrine Cartwright'
+    },
+    {
+      id: 29,
+      name: 'Chasity Sipes'
+    },
+    {
+      id: 30,
+      name: 'Talon Bayer'
+    },
+    {
+      id: 31,
+      name: 'Elinor Greenholt'
+    },
+    {
+      id: 32,
+      name: 'Milford Ryan'
+    },
+    {
+      id: 33,
+      name: 'Zion Beer'
+    },
+    {
+      id: 34,
+      name: 'Maxie Gulgowski'
+    },
+    {
+      id: 35,
+      name: 'Brock Hilpert'
+    },
+    {
+      id: 36,
+      name: 'Torrey Feest'
+    },
+    {
+      id: 37,
+      name: 'Lula Padberg'
+    },
+    {
+      id: 38,
+      name: 'Darius West'
+    },
+    {
+      id: 39,
+      name: 'Lyla Pfannerstill'
+    },
+    {
+      id: 40,
+      name: 'Wilhelmine Schamberger'
+    },
+    {
+      id: 41,
+      name: 'Electa Ratke'
+    },
+    {
+      id: 42,
+      name: 'Dan Gusikowski'
+    },
+    {
+      id: 43,
+      name: 'Linwood Champlin'
+    },
+    {
+      id: 44,
+      name: 'Rylan Hilll'
+    },
+    {
+      id: 45,
+      name: 'Maiya Ratke'
+    },
+    {
+      id: 46,
+      name: "Cayla O'Conner"
+    },
+    {
+      id: 47,
+      name: 'Oran Pouros'
+    },
+    {
+      id: 48,
+      name: 'Chloe Deckow'
+    },
+    {
+      id: 49,
+      name: 'Randall Tillman'
+    },
+    {
+      id: 50,
+      name: 'Victor Jerde'
+    },
+    {
+      id: 51,
+      name: 'Jada Spencer'
+    },
+    {
+      id: 52,
+      name: 'Lula Jakubowski'
+    },
+    {
+      id: 53,
+      name: 'Hazel Marks'
+    },
+    {
+      id: 54,
+      name: 'Jaiden Bechtelar'
+    },
+    {
+      id: 55,
+      name: 'Selmer Dietrich'
+    },
+    {
+      id: 56,
+      name: 'Ruth Veum'
+    },
+    {
+      id: 57,
+      name: 'Emmanuel Emmerich'
+    },
+    {
+      id: 58,
+      name: 'Dean Bayer'
+    },
+    {
+      id: 59,
+      name: 'Sanford Reilly'
+    },
+    {
+      id: 60,
+      name: 'Hollis Bailey'
+    },
+    {
+      id: 61,
+      name: 'Tressa Jakubowski'
+    },
+    {
+      id: 62,
+      name: 'Terry Kuhn'
+    },
+    {
+      id: 63,
+      name: 'Edgardo Kovacek'
+    },
+    {
+      id: 64,
+      name: 'Karina Pfeffer'
+    },
+    {
+      id: 65,
+      name: 'Miller Kihn'
+    },
+    {
+      id: 66,
+      name: 'Eunice Cruickshank'
+    },
+    {
+      id: 67,
+      name: 'Dale Volkman'
+    },
+    {
+      id: 68,
+      name: 'Darrick Jakubowski'
+    },
+    {
+      id: 69,
+      name: 'Rollin Kuphal'
+    },
+    {
+      id: 70,
+      name: 'Jordyn Keeling'
+    },
+    {
+      id: 71,
+      name: 'Jerome Bogan'
+    },
+    {
+      id: 72,
+      name: 'Cleora Barton'
+    },
+    {
+      id: 73,
+      name: 'Davin Hoeger'
+    },
+    {
+      id: 74,
+      name: 'Santina Morar'
+    },
+    {
+      id: 75,
+      name: 'Filomena Fadel'
+    },
+    {
+      id: 76,
+      name: 'Finn Klocko'
+    },
+    {
+      id: 77,
+      name: 'Zakary Kuhic'
+    },
+    {
+      id: 78,
+      name: "Elisabeth O'Hara"
+    },
+    {
+      id: 79,
+      name: 'Brandyn Ortiz'
+    },
+    {
+      id: 80,
+      name: 'Antwan Grimes'
+    },
+    {
+      id: 81,
+      name: 'Ignacio Strosin'
+    },
+    {
+      id: 82,
+      name: 'Wilber Baumbach'
+    },
+    {
+      id: 83,
+      name: 'Edmund Connelly'
+    },
+    {
+      id: 84,
+      name: 'Rodolfo Johnson'
+    },
+    {
+      id: 85,
+      name: 'Kelli Dare'
+    },
+    {
+      id: 86,
+      name: 'Remington Erdman'
+    },
+    {
+      id: 87,
+      name: 'Ahmed Orn'
+    },
+    {
+      id: 88,
+      name: 'Bethel Rosenbaum'
+    },
+    {
+      id: 89,
+      name: 'Abdullah Schimmel'
+    },
+    {
+      id: 90,
+      name: 'Furman Parker'
+    },
+    {
+      id: 91,
+      name: 'Ernesto Corkery'
+    },
+    {
+      id: 92,
+      name: 'Ilene Huels'
+    },
+    {
+      id: 93,
+      name: 'Clovis Durgan'
+    },
+    {
+      id: 94,
+      name: 'Alexandro Swift'
+    },
+    {
+      id: 95,
+      name: 'Alia Fadel'
+    },
+    {
+      id: 96,
+      name: 'Zander Hammes'
+    },
+    {
+      id: 97,
+      name: 'Nichole Kilback'
+    },
+    {
+      id: 98,
+      name: 'Evans Rath'
+    },
+    {
+      id: 99,
+      name: 'Alana Kutch'
+    },
+    {
+      id: 100,
+      name: 'Riley Mann'
     }
   ],
   rooms: [
